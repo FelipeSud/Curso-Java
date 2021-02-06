5ff2c81f975459f8621fe1c5b83b4c3ef4636374
Felipe dos Santos <felipesantos.dsb@hotmail.com>
2021-02-05 21:58:42 -0300

    Criação do repositório e envio de arquivos

---
+++ Digital Innovation One/src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Controller {
+
+var string = new String (s: " Minha String ");
+
+System.out.println("A B C D E F G".toCharArray());
+System.out.println("Aula de Java".split(" "));
+System.out.println("Aula".concat("de Java"));
+System.out.println("1234 asda qw".replaceAll("[0-9]", "#"));
+
+}
