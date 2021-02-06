0898f5bc3d8abb8281086c57e453138e2d56d19f
Felipe dos Santos <felipesantos.dsb@hotmail.com>
2021-02-05 22:04:33 -0300

    Mais arquivos

---
+++ Cod3r/Exercicio-String/src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		var string = " Minha String ";
+
+		System.out.println("A B C D E F G".toCharArray());
+		System.out.println("Aula de Java".split(" "));
+		System.out.println("Aula".concat("de Java"));
+		System.out.println("1234 asda qw".replaceAll("[0-9]", "#"));
+
+	}
+
+}
